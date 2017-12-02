d00bfb9f1374587000be2c2c406b11e6c29c6b66
chibiskye <mongting.gao@stonybrook.edu>
2017-12-02 13:20:34 -0500

    comments & small UI adjustments

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+// test print
 var form = document.getElementById("image-form");
 form.onsubmit = function(e) {
 	e.preventDefault();
 	console.log(form.test.value);
 };
 
+// checks for valid API Key
 try {
   var app = new Clarifai.App({
     apiKey: myApiKey
@@ -15,11 +17,11 @@ catch(err) {
 }
 
 // predict the contents of an image by passing in a url
-	app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-        function(response) {
-          console.log(response);
-        },
-        function(err) {
-          console.error(err);
-        }
-      );
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+	function(response) {
+		console.log(response);
+	},
+	function(err) {
+		console.error(err);
+		}
+	);
