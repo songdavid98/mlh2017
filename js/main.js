ae9135787e8f9cae3d6bdca5a513ab28922d80f9
songdavid98 <songdavid98@gmail.com>
2017-12-02 10:18:54 -0500

    added clarifai for js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// instantiate a new Clarifai app passing in your api key.
+      const app = new Clarifai.App({
+       apiKey: 'a84ff7aedb104f63adfb6f5469713831'
+      });
+      
+      // predict the contents of an image by passing in a url
+      app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+        function(response) {
+          console.log(response);
+        },
+        function(err) {
+          console.error(err);
+        }
+      );
