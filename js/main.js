4555f23555107fda3bcc916232b9e35d9fc1d0a9
chibiskye <mongting.gao@stonybrook.edu>
2017-12-02 11:19:28 -0500

    jenny's changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+// Install the client from NPM
+npm install clarifai
+
+// Require the client
+const Clarifai = require('clarifai');
+
 // instantiate a new Clarifai app passing in your api key.
-      const app = new Clarifai.App({
-       apiKey: 'a84ff7aedb104f63adfb6f5469713831'
-      });
-      
-      // predict the contents of an image by passing in a url
-      app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+const app = new Clarifai.App(
+ apiKey: 'a84ff7aedb104f63adfb6f5469713831'
+);
+
+// predict the contents of an image by passing in a url
+	app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
         function(response) {
           console.log(response);
         },
