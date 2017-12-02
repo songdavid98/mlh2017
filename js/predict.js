616a71e7f77dfed77e598ff817dfe6d39c7a3e5c
chibiskye <mongting.gao@stonybrook.edu>
2017-12-02 14:56:13 -0500

    output

--- js/predict.js
+++ js/predict.js
@@ -2,9 +2,21 @@ function doPredictURL(value) {
     app.models.predict(Clarifai.GENERAL_MODEL, value).then(
         function (response) {
             console.log(response);
+			// make sense of response
+			var output = "<p>";
+			
+			var conceptNames = "";
+			var tagArray;
+			var tagCount = 0;
+			
+			var outputStatus = "Status code: " + response.rawData.status.code + "\nDescription: " + response.rawData.status.description;
+			output += outputStatus + "</p>";
+			
+			maindiv = document.getElementById("result");
+			maindiv.innerHTML = outputStatus;
         },
         function (err) {
-            console.error(err);
+            alert('Unable to predict');
         }
     );
 
