9043107ef5d6c836729bafac7a53a69c2dd04bec
chibiskye <mongting.gao@stonybrook.edu>
2017-12-02 16:19:25 -0500

    added more keywords

--- js/predict.js
+++ js/predict.js
@@ -1,10 +1,13 @@
-function doPredictURL(value) {
+function doPredict(value) {
     app.models.predict(Clarifai.GENERAL_MODEL, value, {
         maxConcepts: 45
     }).then(
         function (response) {
             console.log(response);
-            // make sense of response
+			
+			var keywords = ["weapon", "monster", "scary", "fear", "horror",
+							"nightmare", "knife", "blood", "bloody", "creepy",
+							"dangerous", "danger"];
             var output = "<b>Output:</b><br>";
 
             var outputStatus = "Status code: " +
@@ -21,22 +24,28 @@ function doPredictURL(value) {
                 "<br>app_id: " +
                 outputArray.model.app_id;
             // add to output string
-            output += outputModel + "<br><br>";
-
-            var imgURL = "Image URL: " +
-                outputArray.input.data.image.url;
-            // add to output string
-            output += imgURL + "<br><br>";
+            output += outputModel + "<br>-------------------------------<br>";
 
             var regionArray = outputArray.data.concepts;
+			var dangerCount = 0;
 
             for (var i = 0; i < regionArray.length; i++) {
-                console.log(regionArray[i].name);
-                console.log(regionArray[i].value);
-
-                var outputTagVal = "Tag: " + regionArray[i].name + "<br>Value: " + regionArray[i].value;
-                output += outputTagVal + "<br><br>";
-            }
+				for (var j = 0; j < keywords.length; j++) {
+					// str1.localeCompare(str2) returns 0 if same string
+					if (regionArray[i].name.localeCompare(keywords[j]) == 0) {
+						dangerCount += regionArray[i].value;
+						output += "<b>Danger Percentage: " + dangerCount + "</b><br>";
+					}
+				}
+				
+				var outputTagVal = "Tag: "
+				+ regionArray[i].name
+				+ "<br>Value: "
+				+ regionArray[i].value;
+				
+				// add to output string
+				output += outputTagVal + "<br><br>";
+			}
 
             maindiv = document.getElementById("result");
             maindiv.innerHTML = output;
