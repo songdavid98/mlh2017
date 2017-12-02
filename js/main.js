6cea8520a15c497122a24bf1122efc019a40a026
songdavid98 <songdavid98@gmail.com>
2017-12-02 12:51:20 -0500

    rearranged code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ form.onsubmit = function(e) {
 	console.log(form.test.value);
 };
 
+try {
+  var app = new Clarifai.App({
+    apiKey: myApiKey
+  });
+}
+catch(err) {
+  alert("Need a valid API Key!");
+  throw "Invalid API Key";
+}
 
 // predict the contents of an image by passing in a url
 	app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
